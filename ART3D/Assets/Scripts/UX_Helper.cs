848fe115d2e01a016b2f23bbc37167c97e26ce82
H <xhce911@gmail.com>
2020-09-01 17:01:19 -0500

    adding UI elements

---
+++ ART3D/Assets/Scripts/UX_Helper.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UX_Helper : MonoBehaviour
+{
+    public GameObject Dock;
+    public GameObject[] Controls;
+    public bool state = true;
+
+    public void expandDock()
+    {
+        if (state)
+        {
+            state = false;
+            Debug.Log("on");
+            LeanTween.scaleY(Dock, 6, 2).setEaseInOutCirc();
+        }
+        else
+        {
+            state = true;
+            Debug.Log("off");
+            LeanTween.scaleY(Dock, 1, 2).setEaseOutExpo();
+
+
+        }
+        //;
+    }
+    private void Start()
+    {
+        
+    }
+    //Dock Behavior
+
+    private void Update()
+    {
+    }
+}
