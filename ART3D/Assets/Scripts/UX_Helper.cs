5077c713cda1b5e5508866dfa6536d40da669dbb
HCE <xhce911@gmail.com>
2020-11-10 16:12:04 -0600

    checkpoint UI

--- ART3D/Assets/Scripts/UX_Helper.cs
+++ ART3D/Assets/Scripts/UX_Helper.cs
@@ -3,34 +3,75 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class UX_Helper : MonoBehaviour
 {
+
+    public Button Close;
+    public Button Info;
+    public Button Menu;
+
+    public Button Instrucciones;
+    public Button Politica;
+    public Button Aviso;
+
+
+
     public GameObject Dock;
+    public GameObject ExpandMenu;
     public GameObject[] Controls;
     public bool state = true;
+    public GameObject InfoMenu;
 
-    public void expandDock()
+    public void ShowDock()
     {
         if (state)
         {
             state = false;
             Debug.Log("on");
-            LeanTween.scaleY(Dock, 6, 2).setEaseInOutCirc();
+            Vector3 pos = transform.position;
+            pos.y = -1080;
+            LeanTween.moveLocalY(Dock, pos.y, 2).setEaseInOutCirc();
         }
         else
         {
             state = true;
             Debug.Log("off");
-            LeanTween.scaleY(Dock, 1, 2).setEaseOutExpo();
+            Vector3 pos = transform.position;
+            pos.y = -1280;
+            LeanTween.moveLocalY(Dock, pos.y, 2).setEaseOutExpo();
 
 
         }
         //;
     }
+
+    public void ShowMenu()
+    {
+        ExpandMenu.SetActive(true);
+        LeanTween.alpha(ExpandMenu, 1f, 2f).setEase(LeanTweenType.linear);
+        Debug.Log("Suposely show");
+    }
+
+    public void HideMenu()
+    {
+        ExpandMenu.SetActive(false);
+        LeanTween.alpha(ExpandMenu, 2f, 1f).setEase(LeanTweenType.linear);
+        Debug.Log("Suposely Hide");
+    }
+
+    public void infoMenu()
+    {
+        ExpandMenu.SetActive(true);
+        InfoMenu.SetActive(true);
+      
+    }
+
     private void Start()
     {
+        ExpandMenu.SetActive(false);
+        InfoMenu.SetActive(false);
         
     }
     //Dock Behavior
